2926e7a66c80bcddf14eef07bc3235445ee915e2
francivaldolima <francivaldolimadesousa@gmail.com>
2022-02-28 10:20:18 -0300

    Projeto criado

---
+++ src/main/java/one/digitalinnovetion/personapi/controller/PersonController.java
@@ -0,0 +1,16 @@
+package one.digitalinnovetion.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/people")
+public class PersonController {
+
+    @GetMapping
+    public String getBook(){
+        return "API Test";
+    }
+
+}
